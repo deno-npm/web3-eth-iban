00b92be0081ad4202933d1e047a020407a8f1614
Nicolas Guerrero <stephenguerrero43@gmail.com>
2020-09-13 12:24:39 -0500

    Bump web3-utils import

--- deps.js
+++ deps.js
@@ -1,2 +1,2 @@
 export { default as BN } from "https://cdn.skypack.dev/bn.js@5.1.3";
-export * as utils from "https://deno.land/x/npm_web3_utils@1.2.11/mod.js";
+export * as utils from "https://deno.land/x/npm_web3_utils@1.2.12/mod.js";
